ec1f35f8c8a40739b5624d52468a57e9dee74b5d
unknown <patrickhoffmanncampos@gmail.com>
2025-06-16 19:53:09 -0300

    Conexão com mongoDB ajustada

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
-if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://nodejs-blog-project:u_z4n2GCfM.QmVk@project-nodejs.lbcdq3b.mongodb.net/?retryWrites=true&w=majority&appName=project-nodejs'}
-}else{
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
+module.exports = {
+    mongoURI: process.env.MONGO_URI || 'mongodb://localhost/blogapp'
 }
