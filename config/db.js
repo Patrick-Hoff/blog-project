eda1e46ac43e0b5e05c352af96c507f0e45be4aa
Patrick Hoffmann <patrickhoffmanncampos@gmail.com>
2025-06-16 18:27:54 -0300

    Primeiro commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://patrickhoffmanncampos:2qKNF3ui1E35BW95@project-nodejs.lbcdq3b.mongodb.net/?retryWrites=true&w=majority&appName=project-nodejs'}
+    module.exports = {mongoURI: 'mongodb+srv://nodejs-blog-project:u_z4n2GCfM.QmVk@project-nodejs.lbcdq3b.mongodb.net/?retryWrites=true&w=majority&appName=project-nodejs'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
